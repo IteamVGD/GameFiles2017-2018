6175721dc515ef40d3150f449fffe25f9665b7ec
Gabriel Moncau (Desktop) <gabriel.moncau@hotmail.com>
2018-02-22 20:57:06 -0500

    Added ability for camera to track player vertically in sidescroller if he goes of screen above, increased darkness in the death pits, buffed enemy a bit (faster punch, faster movement, lower delay between movement choices)

--- TSA Game 2017-2018/Assets/Scripts/CoreCtrls/CameraController.cs
+++ TSA Game 2017-2018/Assets/Scripts/CoreCtrls/CameraController.cs
@@ -9,16 +9,29 @@ public class CameraController : MonoBehaviour {
     public Vector3 offset;
     public bool followY; //If the camera should track the player on the yAxis; If in sidescroll mode, no, topdown yes
     public Vector3 desiredPostion;
+    public float yTrackHeight; //The y coordinate the player has to be at for the camera to start tracking him vertically
 
     private void FixedUpdate()
     {
-        if(followY)
+        if (playerObj.transform.GetComponent<PlayerController>().gameControllerScript.currentView == 2 && !followY && playerObj.transform.position.y > yTrackHeight)
+        {
+            followY = true;
+        }
+        else
+        {
+            if (playerObj.transform.GetComponent<PlayerController>().gameControllerScript.currentView == 2 && followY && playerObj.transform.position.y < yTrackHeight)
+            {
+                followY = false;
+            }
+        }
+
+        if (followY)
         {
             desiredPostion = new Vector3(playerObj.transform.position.x + offset.x, playerObj.transform.position.y + offset.y, -10);
         }
         else
         {
-            desiredPostion = new Vector3(playerObj.transform.position.x + offset.x, gameObject.transform.position.y + offset.y, -10);
+            desiredPostion = new Vector3(playerObj.transform.position.x + offset.x, 0 + offset.y, -10);
         }
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPostion, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
